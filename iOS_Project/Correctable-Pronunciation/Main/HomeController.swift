92afa217a1f5c11cc3f5c60d8232e56e9e0f8b78
joseph704 <freelove54@naver.com>
2020-12-10 23:15:38 +0900

    firebase 로그인 구현 및 필요한 동영상 리소스 추가

---
+++ iOS_Project/Correctable-Pronunciation/Main/HomeController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeController.swift
+//  Correctable-Pronunciation
+//
+//  Created by 차요셉 on 2020. 12. 10..
+//  Copyright © 2020년 차요셉. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class HomeController: UIViewController {
+
+    @IBOutlet weak var userlabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let email = Auth.auth().currentUser?.email else {return}
+        userlabel.text = "\(email)님"
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func LogOut(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS_Project/Correctable-Pronunciation/Main/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Correctable-Pronunciation
+//
+//  Created by 차요셉 on 2020. 12. 12..
+//  Copyright © 2020년 차요셉. All rights reserved.
+//
+import UIKit
+import Firebase
+class ViewController: UIViewController { 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func signin(_ sender: Any) {
+        
+        if self.email.text == "" {
+            print("아이디가 입력되지 않았습니다")
+            return
+        }
+        else {
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (authResult, error) in
+            if error != nil {
+                Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!) { [weak self] user, error in
+                    guard let self = self else { return }
+                    self.performSegue(withIdentifier: "Home", sender: nil)
+                }
+            }
+            else {
+                let alert = UIAlertController(title: "알림", message: "회원가입완료", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                self.performSegue(withIdentifier: "Home", sender: nil)
+            }
+        }
+        }
+    }
+}
